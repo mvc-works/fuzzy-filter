a1e429a967dc6cc39df99bb885bae12f8d9a765b
jiyinyiyong <jiyinyiyong@gmail.com>
2018-09-24 23:49:29 +0800

    conflate parsed chunks

--- src/fuzzy_filter/core.cljs
+++ src/fuzzy_filter/core.cljs
@@ -1,9 +1,21 @@
 
 (ns fuzzy-filter.core )
 
+(defn conflate-chunks
+  ([xs] (conflate-chunks [] nil xs))
+  ([acc buffer xs]
+   (if (empty? xs)
+     (if (nil? buffer) acc (conj acc buffer))
+     (let [x0 (first xs)]
+       (if (nil? buffer)
+         (recur acc x0 (rest xs))
+         (if (= (first buffer) (first x0))
+           (recur acc [(first buffer) (str (last buffer) (last x0))] (rest xs))
+           (recur (conj acc buffer) x0 (rest xs))))))))
+
 (defn resolve-text
   ([text query]
-   (let [results (resolve-text [] (seq text) (seq query))]
+   (let [results (conflate-chunks (resolve-text [] (seq text) (seq query)))]
      {:result (not (some (fn [x] (= :missed (first x))) results)), :sequences results}))
   ([acc xs ys]
    (if (empty? xs)
